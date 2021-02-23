40c0bd9c23951db51e68e4c74f7bef83a58faa2b
ajeetjaiswal02 <ajeetjaiswal103@gmail.com>
2021-02-23 17:39:03 +0530

    23 feb 2021 update

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -10,7 +10,17 @@ function LoginScreen(){
                   src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
                   alt="Netflix_logo"                
                 />
-
+                <button className="loginScreen_button">
+                    Sign In
+                </button>
+                <div className="loginScreen_gradient">
+                </div>
+            </div>
+            <div className="loginScreen_body">
+                    <>
+                    <h1>Unlimited films,TV programmes and more.</h1>
+                    <h2>Watch Anywhere,Cancel Anytime</h2>
+                    </>
             </div>
         </div>
     )
