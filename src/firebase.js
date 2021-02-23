671fc84c186d1e8a3296e04b208e487688cd3f87
ajeetjaiswal02 <ajeetjaiswal103@gmail.com>
2021-02-23 14:58:29 +0530

    update

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHOSIZb41PW4grMI1B_cSO8_X7mEq-4B8",
+    authDomain: "netflix-clone-4cca9.firebaseapp.com",
+    projectId: "netflix-clone-4cca9",
+    storageBucket: "netflix-clone-4cca9.appspot.com",
+    messagingSenderId: "1077109780252",
+    appId: "1:1077109780252:web:0390530b0d66e75351d4d9"
+};
