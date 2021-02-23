cc75f92fb2cd60a43655dd792034637553f46913
ajeetjaiswal02 <ajeetjaiswal103@gmail.com>
2021-02-24 00:45:11 +0530

    midnight push

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react"
+import { useHistory } from "react-router-dom";
 import "./Nav.css"
+
 
 
 function Nav(){
     const [show,handleShow] = useState(false);
+    const history = useHistory();   
+
+
     const trasitionNavBar = () => {
         if (window.scrollY > 100) {
             handleShow(true);
@@ -22,10 +27,13 @@ function Nav(){
         <div className={`nav ${show && "nav_black"}`}>
             <div className="nav_contents">
                 <img className="nav_logo" 
+                onClick={() => history.push("/")}
                 src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
                 alt="netflix-logo"
                 />
-                <img className="nav_avatar"
+                <img
+                onClick={() => history.push("/profile")} 
+                className="nav_avatar"
                 src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" 
                 />
             </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from  "firebase";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyBHOSIZb41PW4grMI1B_cSO8_X7mEq-4B8",
     authDomain: "netflix-clone-4cca9.firebaseapp.com",
@@ -6,3 +9,10 @@ const firebaseConfig = {
     messagingSenderId: "1077109780252",
     appId: "1:1077109780252:web:0390530b0d66e75351d4d9"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth();
+
+export { auth }
+export default db;
